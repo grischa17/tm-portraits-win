4de27e5e101745c3f8380bd2ab991c7f2df9c7df
richard-martens <richard.martens@outlook.com>
2016-01-13 10:03:33 +0100

    kkk

---
+++ LetPaintPictures/Models/BillCancellation.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LetPaintPictures.Models
+{
+    public class BillCancellation
+    {
+        [Key, ForeignKey(nameof(Head))]
+        public int Id { get; set; }
+        
+        public BillHead Head { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Reason { get; set; }
+    }
+}
