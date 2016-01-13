00dbf6b84b214c03b096eef4a98c8e7c28562d3b
richard-martens <richard.martens@outlook.com>
2016-01-13 09:43:43 +0100

    3 Dateien vergessen

---
+++ LetPaintPictures/Controllers/BillCancellationController.cs
@@ -0,0 +1,26 @@
+﻿using LetPaintPictures.Models;
+using System.Web.Mvc;
+
+namespace LetPaintPictures.Controllers
+{
+    [Authorize]
+    public class BillCancellationController : Controller
+    {
+
+        public ActionResult Create(int BillId, string CancelReason)
+        {
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                BillCancellation cancellation = new BillCancellation();
+
+                cancellation.Id = BillId;
+                cancellation.Reason = CancelReason;
+
+                db.BillCancellations.Add(cancellation);
+                db.SaveChanges();
+            }
+
+            return View();
+        }
+    }
+}
