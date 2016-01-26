7a077c4fe47da97bb5c97de794b17657516d6635
richard-martens <richard.martens@outlook.com>
2016-01-26 09:23:04 +0100

    2.0.16 2.0.17

--- LetPaintPictures/App_Start/BundleConfig.cs
+++ LetPaintPictures/App_Start/BundleConfig.cs
@@ -9,20 +9,24 @@ namespace LetPaintPictures
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.min.js"
+                        "~/Scripts/jquery-2.1.4.min.js",
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/bootstrap-slider.min.js",
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/respond.min.js"
                         ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*",
-                        "~/Scripts/bootstrap-slider.min.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*",
+            //            "~/Scripts/bootstrap-slider.min.js"));
             // Verwenden Sie die Entwicklungsversion von Modernizr zum Entwickeln und Erweitern Ihrer Kenntnisse. Wenn Sie dann
             // für die Produktion bereit sind, verwenden Sie das Buildtool unter "http://modernizr.com", um nur die benötigten Tests auszuwählen.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.min.js",
-                      "~/Scripts/respond.min.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.min.js",
+            //          "~/Scripts/respond.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap/bootstrap.site.css",
