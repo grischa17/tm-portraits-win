c5913434553ae131fbb96fe1e8210ab10ee3ea80
richard-martens <richard.martens@outlook.com>
2016-03-14 11:10:24 +0100

    vor dem Kopieren

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -4,7 +4,7 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(TuRM.Portrait.Startup))]
 namespace TuRM.Portrait
 {
-    public partial class Startup
+    public partial class Startup : User.Startup
     {
         public void Configuration(IAppBuilder app)
         {
